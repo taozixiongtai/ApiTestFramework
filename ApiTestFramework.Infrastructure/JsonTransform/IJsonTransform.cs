2ec469149c43ec556f0a288bdf64954104410a97
wjt <wjt@DESKTOP-BF2PUBU>
2026-03-03 23:04:45 +0800

    修改transfrom

---
+++ ApiTestFramework.Infrastructure/JsonTransform/IJsonTransform.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTestFramework.Infrastructure.JsonTransform
+{
+    public interface IJsonTransform
+    {
+
+        public string Transform(string json);
+    }
+}
---
+++ ApiTestFramework.Infrastructure/JsonTransform/SnowIdTransfrom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTestFramework.Infrastructure.JsonTransform
+{
+    internal class SnowIdTransfrom : IJsonTransform
+    {
+        public string Transform(string json)
+        {
+          
+        }
+    }
+}
