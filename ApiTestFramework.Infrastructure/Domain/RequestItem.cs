ae1bf8156961ffc31b175073fe4b7aff36afcd39
wjt <wjt@DESKTOP-BF2PUBU>
2026-03-10 22:48:00 +0800

    3.10 写入请求项

---
+++ ApiTestFramework.Infrastructure/Domain/RequestItem.cs
@@ -0,0 +1,34 @@
+﻿using ApiTestFramework.Infrastructure.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTestFramework.Infrastructure.Domain
+{
+    /// <summary>
+    /// 请求项
+    /// </summary>
+    public class RequestItem
+    {
+        /// <summary>
+        /// 请求动词
+        /// </summary>
+        public RequestVerbEnum RequestVerb { set; get; } = RequestVerbEnum.None;
+
+        /// <summary>
+        /// 请求路径
+        /// </summary>
+        public string Path { set; get; } = string.Empty;
+
+        /// <summary>
+        ///  请求体
+        /// </summary>
+        public string Body { set; get; } = string.Empty;
+
+        /// <summary>
+        /// 请求头
+        /// 会和全局请求头进行合并，全局请求头优先级更高
+        /// </summary>
+        public Dictionary<string, string> Header { set; get; } = [];
+    }
+}
---
+++ ApiTestFramework.Infrastructure/Enum/RequestVerbEnum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ApiTestFramework.Infrastructure.Enum
+{
+    public enum RequestVerbEnum
+    {
+        /// <summary>
+        /// 无
+        /// </summary>
+        [Description("无")]
+        None = 0,   
+
+        /// <summary>
+        /// GET 
+        /// </summary>
+        [Description("GET")]
+        Get = 1,
+
+        /// <summary>
+        /// POST 
+        /// </summary>
+        [Description("POST")]
+        Post = 2,
+
+        /// <summary>
+        /// PUT 
+        /// </summary>
+        [Description("PUT")]
+        Put = 3,
+
+        /// <summary>
+        /// DELETE 
+        /// </summary>
+        [Description("DELETE")]
+        Delete = 4,
+
+        /// <summary>
+        /// PATCH 
+        /// </summary>
+        [Description("PATCH")]
+        Patch = 5,
+    }
+}
